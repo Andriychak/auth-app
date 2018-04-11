f4ac3ef5c0f5fb50f0d49a811c68151488cf5520
Volodymyr Andriychak <andriychak@users.noreply.github.com>
2018-04-11 13:17:17 +0300

    initial commit

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 const Event = require('../models/event');
@@ -27,7 +28,9 @@ router.post('/register', (req, res) => {
         if (err) {
             console.log('Error! '+err);
         } else {
-            res.status(200).send(registeredUser);
+            let payload = {subject: registeredUser._id};
+            let token = jwt.sign(payload, 'secretKey');
+            res.status(200).send({token});
         }
     });
 });
@@ -45,7 +48,9 @@ router.post('/login', (req, res) => {
                 if (user.password !== userData.password) {
                     res.status(401).send('Invalid password');
                 } else {
-                    res.status(200).send(user);
+                    let payload = {subject: user._id};
+                    let token = jwt.sign(payload, 'secretKey');
+                    res.status(200).send({token});
                 }
             }
         }
